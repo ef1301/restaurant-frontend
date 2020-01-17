55a395f930ed437e0c9c5da4d9571606e90341c5
Bobur Hurramov <32942527+trekkatlant@users.noreply.github.com>
2020-01-17 16:48:42 -0500

    Update and rename Sign.js to Signin.js

--- src/components/views/Sign.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Sign extends Component {
-
-    render() {
-
-
-    }
-}
---
+++ src/components/views/Signin.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Signin extends Component {
+
+    render() {
+        return()
+    }
+}
+
+export default Signin;
+
+

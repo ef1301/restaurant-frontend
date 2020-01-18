4922bee5dba6ff8fec945da0e5c9f31386f42585
Arman Berek <armanberek@MacBook-Air-10733.local>
2020-01-18 14:20:20 -0500

    finished landing page

--- src/components/views/Landing.js
+++ src/components/views/Landing.js
@@ -8,12 +8,15 @@ class Landing extends Component{
     render(){
         <div className="wrapper">
             <Navbar />
+            <div className="header">
+                <h1>Welcome to "RESTAURANT NAME"</h1>
+            </div>
             <div className="option">
                 <Link to="/">
-                <button type="submit" className="sign-up"></button>
+                <button type="submit" className="sign-up">Sign Up</button>
                 </Link>
                 <Link to="/">
-                <button type="submit" className="log-in"></button>
+                <button type="submit" className="log-in">Log In</button>
                 </Link>
             </div>
         </div>

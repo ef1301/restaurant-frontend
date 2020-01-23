c94349dc722bc93cc9396e47f1acb4a1a3a0a99b
Bobur <bobur.hu0991@gmail.com>
2020-01-23 15:24:18 -0500

    account info

--- src/components/views/Navbar.js
+++ src/components/views/Navbar.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import "../styles/Navbar.css";
-function Navbar({ history }){
+class Navbar extends Component{
 
-    let email = sessionStorage.getItem("email");
+    //let email = sessionStorage.getItem("email");
 
-    const handleSignout = () => {
+    handleSignout = () => {
         sessionStorage.removeItem("byteME");
         sessionStorage.removeItem("email");
         sessionStorage.removeItem("id");
-        history.push("/");
+        //history.push("/");
     };
+    render() {
     return (
 	    <div className="header">
 	    <div id="Byte-me">
@@ -19,13 +20,14 @@ function Navbar({ history }){
             <Link to="/"><img id="house-icon" src="https://img.icons8.com/bubbles/50/000000/order-delivered.png" style={{height: '2em', marginTop: '-5%'}} alt="House-icon"/>Home</Link>
 	    
             <div id="right-align">
-	        <Link to="/acccountInfo">Account</Link>
+	        <Link to="/accountInfo">Account</Link>
             <Link to="/cart">Cart</Link>
-            <Link to="/" onClick={handleSignout}> Sign Out </Link>
+            <Link to="/" onClick={this.handleSignout}> Sign Out </Link>
             </div>
             </div>
 	    </div>
     );
+    }
 }
 export default Navbar;
 //Added link to Cart
